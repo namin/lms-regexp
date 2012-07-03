436b8d3f4f603bf7248a0838a9c111b44f07ea58
Nada Amin <nada.amin@epfl.ch>
2012-07-03 13:00:34 +0200

    checkin generated LMS code so that benchmark can run independently

---
+++ benchmark/src/main/scala/LMS.scala
@@ -0,0 +1,371 @@
+/*****************************************
+  Emitting Generated Code                  
+*******************************************/
+class MatchAAB extends ((Unit)=>(scala.virtualization.lms.regexp.Automaton[Char, Byte])) {
+def apply(x0:Unit): scala.virtualization.lms.regexp.Automaton[Char, Byte] = {
+var x13 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x1 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x17 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x4 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x10 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x7 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x12 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+x1 = {x2: (Char) => 
+val x3 = x2 == 'A'
+val x18 = if (x3) {
+x17
+} else {
+x13
+}
+x18
+}
+x12 = scala.virtualization.lms.regexp.Automaton(1.toByte,x1)
+x7 = {x8: (Char) => 
+val x9 = x8 == 'A'
+val x15 = if (x9) {
+x10
+} else {
+val x11 = x8 == 'B'
+val x14 = if (x11) {
+x12
+} else {
+x13
+}
+x14
+}
+x15
+}
+x10 = scala.virtualization.lms.regexp.Automaton(0.toByte,x7)
+x4 = {x5: (Char) => 
+val x6 = x5 == 'A'
+val x16 = if (x6) {
+x10
+} else {
+x13
+}
+x16
+}
+x17 = scala.virtualization.lms.regexp.Automaton(0.toByte,x4)
+x13 = scala.virtualization.lms.regexp.Automaton(0.toByte,x1)
+x13
+}
+}
+/*****************************************
+  End of Generated Code                  
+*******************************************/
+/*****************************************
+  Emitting Generated Code                  
+*******************************************/
+class MatchAABany extends ((Unit)=>(scala.virtualization.lms.regexp.Automaton[Char, Byte])) {
+def apply(x19:Unit): scala.virtualization.lms.regexp.Automaton[Char, Byte] = {
+var x42 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x31 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x45 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x34 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x40 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x37 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x47 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x20 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x51 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x23 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x29 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x26 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+x37 = {x38: (Char) => 
+val x39 = x38 == 'A'
+val x43 = if (x39) {
+x40
+} else {
+x42
+}
+x43
+}
+x40 = scala.virtualization.lms.regexp.Automaton(3.toByte,x37)
+x34 = {x35: (Char) => 
+val x36 = x35 == 'A'
+val x44 = if (x36) {
+x40
+} else {
+x42
+}
+x44
+}
+x45 = scala.virtualization.lms.regexp.Automaton(3.toByte,x34)
+x31 = {x32: (Char) => 
+val x33 = x32 == 'A'
+val x46 = if (x33) {
+x45
+} else {
+x42
+}
+x46
+}
+x42 = scala.virtualization.lms.regexp.Automaton(3.toByte,x31)
+x26 = {x27: (Char) => 
+val x28 = x27 == 'A'
+val x49 = if (x28) {
+x29
+} else {
+val x30 = x27 == 'B'
+val x48 = if (x30) {
+x42
+} else {
+x47
+}
+x48
+}
+x49
+}
+x29 = scala.virtualization.lms.regexp.Automaton(0.toByte,x26)
+x23 = {x24: (Char) => 
+val x25 = x24 == 'A'
+val x50 = if (x25) {
+x29
+} else {
+x47
+}
+x50
+}
+x51 = scala.virtualization.lms.regexp.Automaton(0.toByte,x23)
+x20 = {x21: (Char) => 
+val x22 = x21 == 'A'
+val x52 = if (x22) {
+x51
+} else {
+x47
+}
+x52
+}
+x47 = scala.virtualization.lms.regexp.Automaton(0.toByte,x20)
+x47
+}
+}
+/*****************************************
+  End of Generated Code                  
+*******************************************/
+/*****************************************
+  Emitting Generated Code                  
+*******************************************/
+class MatchUSD extends ((Unit)=>(scala.virtualization.lms.regexp.Automaton[Char, Byte])) {
+def apply(x53:Unit): scala.virtualization.lms.regexp.Automaton[Char, Byte] = {
+var x90 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x88 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x76 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x71 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+x88 = {x89: (Char) => 
+x90
+}
+x90 = scala.virtualization.lms.regexp.Automaton(2.toByte,x88)
+val x91 = scala.virtualization.lms.regexp.Automaton(1.toByte,x88)
+val x83 = {x84: (Char) => 
+val x85 = '0' <= x84
+val x86 = x84 <= '9'
+val x87 = x85 && x86
+val x92 = if (x87) {
+x91
+} else {
+x90
+}
+x92
+}
+val x93 = scala.virtualization.lms.regexp.Automaton(0.toByte,x83)
+val x78 = {x79: (Char) => 
+val x80 = '0' <= x79
+val x81 = x79 <= '9'
+val x82 = x80 && x81
+val x94 = if (x82) {
+x93
+} else {
+x90
+}
+x94
+}
+val x95 = scala.virtualization.lms.regexp.Automaton(0.toByte,x78)
+x71 = {x72: (Char) => 
+val x73 = '0' <= x72
+val x74 = x72 <= '9'
+val x75 = x73 && x74
+val x97 = if (x75) {
+x76
+} else {
+val x77 = x72 == '.'
+val x96 = if (x77) {
+x95
+} else {
+x90
+}
+x96
+}
+x97
+}
+x76 = scala.virtualization.lms.regexp.Automaton(0.toByte,x71)
+val x99 = {x100: (Char) => 
+val x101 = '0' <= x100
+val x102 = x100 <= '9'
+val x103 = x101 && x102
+val x104 = if (x103) {
+x76
+} else {
+x90
+}
+x104
+}
+val x105 = scala.virtualization.lms.regexp.Automaton(0.toByte,x99)
+val x66 = {x67: (Char) => 
+val x68 = '0' <= x67
+val x69 = x67 <= '9'
+val x70 = x68 && x69
+val x109 = if (x70) {
+x76
+} else {
+val x98 = x67 == '+'
+val x108 = if (x98) {
+x105
+} else {
+val x106 = x67 == '-'
+val x107 = if (x106) {
+x105
+} else {
+x90
+}
+x107
+}
+x108
+}
+x109
+}
+val x110 = scala.virtualization.lms.regexp.Automaton(0.toByte,x66)
+val x63 = {x64: (Char) => 
+val x65 = x64 == ' '
+val x111 = if (x65) {
+x110
+} else {
+x90
+}
+x111
+}
+val x112 = scala.virtualization.lms.regexp.Automaton(0.toByte,x63)
+val x60 = {x61: (Char) => 
+val x62 = x61 == 'd'
+val x113 = if (x62) {
+x112
+} else {
+x90
+}
+x113
+}
+val x114 = scala.virtualization.lms.regexp.Automaton(0.toByte,x60)
+val x57 = {x58: (Char) => 
+val x59 = x58 == 's'
+val x115 = if (x59) {
+x114
+} else {
+x90
+}
+x115
+}
+val x116 = scala.virtualization.lms.regexp.Automaton(0.toByte,x57)
+val x54 = {x55: (Char) => 
+val x56 = x55 == 'u'
+val x117 = if (x56) {
+x116
+} else {
+x90
+}
+x117
+}
+val x118 = scala.virtualization.lms.regexp.Automaton(0.toByte,x54)
+x118
+}
+}
+/*****************************************
+  End of Generated Code                  
+*******************************************/
+/*****************************************
+  Emitting Generated Code                  
+*******************************************/
+class MatchAnything extends ((Unit)=>(scala.virtualization.lms.regexp.Automaton[Char, Byte])) {
+def apply(x119:Unit): scala.virtualization.lms.regexp.Automaton[Char, Byte] = {
+var x123 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x120 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+x120 = {x121: (Char) => 
+x123
+}
+x123 = scala.virtualization.lms.regexp.Automaton(3.toByte,x120)
+x123
+}
+}
+/*****************************************
+  End of Generated Code                  
+*******************************************/
+/*****************************************
+  Emitting Generated Code                  
+*******************************************/
+class MatchCook extends ((Unit)=>(scala.virtualization.lms.regexp.Automaton[Char, Byte])) {
+def apply(x124:Unit): scala.virtualization.lms.regexp.Automaton[Char, Byte] = {
+var x90 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x88 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x125 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x137 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x131 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x128 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+var x140 = null.asInstanceOf[scala.virtualization.lms.regexp.Automaton[Char, Byte]]
+var x133 = null.asInstanceOf[scala.Function1[Char, scala.virtualization.lms.regexp.Automaton[Char, Byte]]]
+x88 = {x89: (Char) => 
+x90
+}
+x90 = scala.virtualization.lms.regexp.Automaton(2.toByte,x88)
+x125 = {x126: (Char) => 
+val x127 = x126 == 'A'
+val x145 = if (x127) {
+x131
+} else {
+val x143 = x126 == 'B'
+val x144 = if (x143) {
+x137
+} else {
+x90
+}
+x144
+}
+x145
+}
+x137 = scala.virtualization.lms.regexp.Automaton(1.toByte,x125)
+x133 = {x134: (Char) => 
+val x135 = x134 == 'A'
+val x139 = if (x135) {
+x131
+} else {
+val x136 = x134 == 'B'
+val x138 = if (x136) {
+x137
+} else {
+x90
+}
+x138
+}
+x139
+}
+x140 = scala.virtualization.lms.regexp.Automaton(1.toByte,x133)
+x128 = {x129: (Char) => 
+val x130 = x129 == 'A'
+val x142 = if (x130) {
+x131
+} else {
+val x132 = x129 == 'B'
+val x141 = if (x132) {
+x140
+} else {
+x90
+}
+x141
+}
+x142
+}
+x131 = scala.virtualization.lms.regexp.Automaton(1.toByte,x128)
+val x146 = scala.virtualization.lms.regexp.Automaton(0.toByte,x125)
+x146
+}
+}
+/*****************************************
+  End of Generated Code                  
+*******************************************/
